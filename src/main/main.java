08048a13ccdd10f8e6ebe337915edc55227d1b91
evak1n <ertugrulvelia@gmail.com>
2025-12-27 16:34:13 +0300

    init: proje başlatıldı

---
+++ src/main/main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("------------------------------");
+		System.out.println("Arel Bankacılığa Hoş Geldiniz!");
+		System.out.println("------------------------------");
+		//Scanner scanner = new Scanner(System.in);
+		//int tckn = scanner.nextLine();
+		
+	//	System.out.println(tckn);
+		
+		
+	}
+
+}
